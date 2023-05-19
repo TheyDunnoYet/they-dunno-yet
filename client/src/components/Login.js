a84d6dff573edc82648a771a28f477e8f6322ed4
TheyDunnoYet <theydunnoyet@gmail.com>
2023-05-19 17:31:54 +0200

    Fixed login + improved error handling

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,9 +3,10 @@ import { connect } from "react-redux";
 import { login } from "../redux/actions/authActions";
 import { clearErrors } from "../redux/actions/errorActions";
 import { useNavigate } from "react-router-dom";
-import { TextField, Button } from "@material-ui/core";
+import { TextField, Button, Container, Box, Snackbar } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
 
-function Login({ login, clearErrors, error = {} }) {
+function Login({ login, clearErrors, error = {}, isAuthenticated }) {
   let navigate = useNavigate();
 
   const [userData, setUserData] = useState({
@@ -13,62 +14,162 @@ function Login({ login, clearErrors, error = {} }) {
     password: "",
   });
 
-  useEffect(() => {
-    return () => {
-      clearErrors(); // Clear errors when the component is unmounted
-    };
-  }, [clearErrors]);
+  const [formErrors, setFormErrors] = useState({
+    email: "",
+    password: "",
+  });
 
-  const handleChange = (event) => {
-    setUserData({
-      ...userData,
-      [event.target.name]: event.target.value,
-    });
+  const [localErrors, setLocalErrors] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [snackbarOpen, setSnackbarOpen] = useState(false); // For Snackbar
+  const [errorSnackbarOpen, setErrorSnackbarOpen] = useState(false);
+
+  useEffect(() => {
+    if (error && error.id === "LOGIN_FAIL") {
+      setFormErrors({
+        email: error.msg.email,
+        password: error.msg.password,
+      });
+      clearErrors();
+    }
+  }, [error, clearErrors]);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      setSnackbarOpen(true);
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    }
+  }, [isAuthenticated, navigate]);
+
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setUserData({ ...userData, email: newEmail });
+
+    const isValidEmail = (email) => {
+      const re = /\S+@\S+\.\S+/;
+      return re.test(email);
+    };
+
+    if (!newEmail.trim() || !isValidEmail(newEmail)) {
+      setLocalErrors((errors) => ({
+        ...errors,
+        email: "Please enter a valid email.",
+      }));
+    } else {
+      setLocalErrors((errors) => ({
+        ...errors,
+        email: "",
+      }));
+    }
+  };
+
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setUserData({ ...userData, password: newPassword });
+
+    if (!newPassword.trim()) {
+      setLocalErrors((errors) => ({
+        ...errors,
+        password: "Please enter a password.",
+      }));
+    } else {
+      setLocalErrors((errors) => ({
+        ...errors,
+        password: "",
+      }));
+    }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    login(userData)
-      .then(() => {
-        navigate("/"); // redirect user after login
-      })
-      .catch((err) => {
-        // handle error
-      });
+
+    if (localErrors.email || localErrors.password) {
+      return;
+    }
+
+    // Attempt to login
+    login(userData).catch((err) => {
+      if (err.response && err.response.status === 400) {
+        setErrorSnackbarOpen(true); // Open the error Snackbar
+        setFormErrors({
+          ...formErrors,
+          email: "This user doesn't exist. Please try again.",
+          password: "This user doesn't exist. Please try again.",
+        });
+      }
+    });
+  };
+
+  const handleClose = () => {
+    setSnackbarOpen(false);
   };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          type="email"
-          name="email"
-          value={userData.email}
-          onChange={handleChange}
-          label="Email"
-          required
-          error={!!error.email}
-          helperText={error.email}
-        />
-        <TextField
-          type="password"
-          name="password"
-          value={userData.password}
-          onChange={handleChange}
-          label="Password"
-          required
-          error={!!error.password}
-          helperText={error.password}
-        />
-        <Button type="submit">Login</Button>
-      </form>
-    </div>
+    <Container maxWidth="xs">
+      <Box my={5}>
+        <h1 style={{ textAlign: "center" }}>Login</h1>
+        <form onSubmit={handleSubmit} noValidate>
+          <TextField
+            type="email"
+            name="email"
+            value={userData.email}
+            onChange={handleEmailChange}
+            label="Email"
+            error={!!(formErrors.email || localErrors.email)}
+            helperText={formErrors.email || localErrors.email}
+            fullWidth
+          />
+          <TextField
+            type="password"
+            name="password"
+            value={userData.password}
+            onChange={handlePasswordChange}
+            label="Password"
+            error={!!(formErrors.password || localErrors.password)}
+            helperText={formErrors.password || localErrors.password}
+            fullWidth
+          />
+          <Button type="submit" color="primary" variant="contained" fullWidth>
+            Login
+          </Button>
+        </form>
+        <Snackbar
+          open={errorSnackbarOpen}
+          autoHideDuration={5000}
+          onClose={() => setErrorSnackbarOpen(false)}
+        >
+          <Alert onClose={() => setErrorSnackbarOpen(false)} severity="error">
+            This user doesn't exist. Please try again.
+          </Alert>
+        </Snackbar>
+
+        <Snackbar
+          open={snackbarOpen}
+          autoHideDuration={2000}
+          onClose={handleClose}
+        >
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            variant="filled"
+            elevation={6}
+          >
+            Login successful!
+          </Alert>
+        </Snackbar>
+      </Box>
+    </Container>
   );
 }
 
 const mapStateToProps = (state) => ({
-  error: state.error || {},
+  error: state.error,
+  isAuthenticated: state.auth.isAuthenticated,
 });
 
 export default connect(mapStateToProps, { login, clearErrors })(Login);
